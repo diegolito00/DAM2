7069b52fe613d515647dd94090f008b2147ff395
diegolito00 <55633113+diegolito00@users.noreply.github.com>
2023-12-07 16:15:44 +0100

    Refactorizado Persona.cs

---
+++ Persona.cs
@@ -0,0 +1,34 @@
+public class Persona {
+
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public string Dirección { get; set; }
+
+    public void MostrarDatos() {
+        Console.WriteLine("Nombre: {0}", Nombre);
+        Console.WriteLine("Edad: {0}", Edad);
+        Console.WriteLine("Dirección: {0}", Dirección);
+    }
+
+    public void CalcularIMC(int peso, int altura) {
+        if (altura != 0)
+        {
+            float imc = peso / (altura * altura);
+            Console.WriteLine("IMC: {0}", imc);
+        }
+        else 
+        {
+            Console.WriteLine("La altura no puede ser 0");
+        }
+    }
+
+    public void ActualizarDatos() {
+        // Obtiene los datos del usuario
+        Console.WriteLine("Introduce tu nombre:");
+        Nombre = Console.ReadLine();
+        Console.WriteLine("Introduce tu edad:");
+        Edad = int.Parse(Console.ReadLine());
+        Console.WriteLine("Introduce tu dirección:");
+        Dirección = Console.ReadLine();
+    }
+}  
